fdc7a4ea43c2939b8996172a66dc7bc47a72b4c2
stefan <stefan@dl2stg.de>
2017-05-16 00:35:32 +0200

    - upgrade to vivado 2017.1 - fpga filestuctur cleaned - 16 rx in fpga and r2t2srv - noise filter - fixes

--- freertos/trunk/src/.gdbinit
+++
@@ -1,7 +0,0 @@
-set prompt  (zynq-gdb)
-set print pretty
-
-target remote localhost:1235
-b main
-b Xil_Assert
-b main.c:93
--- freertos/trunk/src/release/bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm -r CMake*
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../zynq.cmake -G "Unix Makefiles" ..
